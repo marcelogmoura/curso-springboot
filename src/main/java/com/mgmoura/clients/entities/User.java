af9c257c5a3e6f6a6df30326d89d3e7c270701e5
Marcelo Moura <mgmoura@gmail.com>
2022-08-09 09:59:11 -0300

    User entity And Resource

--- src/main/java/com/mgmoura/clients/entities/User.java
+++ src/main/java/com/mgmoura/clients/entities/User.java
@@ -12,8 +12,7 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
-	public User() {
-		
+	public User() {		
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
